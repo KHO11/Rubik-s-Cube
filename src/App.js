ec5842c33ee8488e5dba5d047309172640f5f979
kevhy <kevhydeopoku@gmail.com>
2024-01-28 21:39:41 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import { useEffect, useState } from 'react';
 
 function App() {
   
-  const [color, setColor] = useState(['#00ff00', '#0000ff', '#ff9900', '#ff0000', '#ffff00', '#ffffff']);
+  const [color, setColor] = useState(['#00ff00', '#ff0000', '#ffffff', '#0000ff', '#ff9900', '#ffff00']);
 
-  const [sides, setSides] = useState(['front', 'back', 'left', 'right', 'bottom', 'top']);
+  const [cubeSides, setCubeSides] = useState(['front', 'right', 'up', 'back', 'left', 'down']); 
 
   useEffect(() => {
     
     let box = document.querySelector('.box');
 
-    for (var i of sides) {
-      let side = document.createElement('div');
-      box.appendChild(side);
-      side.classList.add(i);
+    for (var i of cubeSides) {
+      let eachCubeSide = document.createElement('div');
+      box.appendChild(eachCubeSide);
+      eachCubeSide.classList.add(i);
 
-      side.setAttribute('style', `--clr:${color[sides.indexOf(i)]}`);
+      eachCubeSide.setAttribute('style', `--clr:${color[cubeSides.indexOf(i)]}`);
 
-    for (let j = 0; j < 9; j++) {
-      let span = document.createElement('span');
-      side.appendChild(span);
-    }
+      for (let j = 0; j < 9; j++) {
+        let span = document.createElement('span');
+        eachCubeSide.appendChild(span);
+      }
       
-    side.querySelector('span').setAttribute('style', `--clr:${color[sides.indexOf(i)]}`);
+      eachCubeSide.querySelector('span').setAttribute('style', `--clr:${color[cubeSides.indexOf(i)]}`);
     }
         
     var isKeyDown = false;
@@ -32,26 +32,25 @@ function App() {
 
     document.documentElement.addEventListener('mousedown', function (e) {
       isKeyDown = true;
-      rotateCube(e.clientX, e.clientY) ;
+      rotateCube(e.clientX, e.clientY);
     })
-
 
     document.documentElement.addEventListener('mouseup', function (e) {
       isKeyDown = false;
-      rotateCube(-285, -250);
+      rotateCube(e.clientX, e.clientY);
     })
 
     document.documentElement.addEventListener('mousemove', function (e) {
       isKeyDown && rotateCube(e.clientX, e.clientY)
     })
-    
-    function rotateCube(x, y) {
-      let box = document.querySelector('.box');
-      let Xvalue = Math.floor((x / 2) + 100);
-      let Yvalue = Math.floor((y / 2) + 100);
-      box.style.transform = `rotateX(${Yvalue}deg) rotateY(${Xvalue}deg)`;
-    }
   });
+
+  function rotateCube(x, y) {
+    let box = document.querySelector('.box');
+    let Xvalue = Math.floor((x / 2) + 100);
+    let Yvalue = Math.floor((y / 2) + 100);
+    box.style.transform = `rotateX(${Yvalue}deg) rotateY(${Xvalue}deg)`;
+  }
 
   return (
     <div className="App">
